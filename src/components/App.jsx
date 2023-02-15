87310d05f61b819319dd5e002e8999f423b2cb2d
Manny Cortez <mannycortezstudios@gmail.com>
2023-02-14 23:49:52 -0500

    Added basic routes and movies, movie-info components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
-// import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 const App = () => (
   <div>
     <CssBaseline />
-    <h1> Hello Brave New Movies</h1>
+    <main>
+      <Switch>
+        <Route exact path="/movie/:id">
+          <h1>Movie Information</h1>
+        </Route>
+        <Route exact path="/actors/:id">
+          <h1>Actors</h1>
+        </Route>
+        <Route exact path="/">
+          <h1>Movies</h1>
+        </Route>
+        <Route exact path="/profile/:id">
+          <h1>Profile</h1>
+        </Route>
+      </Switch>
+    </main>
   </div>
 );
 
