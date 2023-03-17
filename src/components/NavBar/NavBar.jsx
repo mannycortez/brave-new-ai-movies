31629508641d01c9144416b11be27e69bfe05e48
Manny Cortez <mannycortezstudios@gmail.com>
2023-03-17 15:28:03 -0400

    Added navBar and styles js file

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery } from '@mui/material';
+import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { ClassNames } from '@emotion/react';
+import useStyles from './styles';
 
 const NavBar = () => {
-  console.log('NavBar');
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width: 600px)');
   return (
-    <div>NavBar</div>
+    // eslint-disable-next-line
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
+            <IconButton
+              color="inherit"
+              edge="start"
+              style={{ outline: 'none' }}
+              onClick={() => {}}
+              className={classes.menuButton}
+            >
+              <Menu />
+            </IconButton>
+          )}
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
